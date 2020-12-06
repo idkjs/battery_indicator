49cb53eac5e358affd2a72c65544b39204470d3f
Alain Armand <aarmand.inbox@gmail.com>
2020-12-06 01:15:35 +0100

    port to fish:WIP

--- functions/battery_indicator.fish
+++ functions/battery_indicator.fish
@@ -1,4 +1,7 @@
 #!/usr/bin/env fish
 set script_dir (realpath (dirname (status --current-filename)))
 
-bass source $script_dir/battery_indicator.sh
+function battery_indicator
+    bass $script_dir/battery_indicator.sh
+end
+
